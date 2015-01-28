b0881b4842d605818a5292c0ac52d504685b6ee7
Steve Bellan <steve.bellan@gmail.com>
2015-01-28 10:51:13 -0600

    select() no longer working, committing to debug

--- simFuns.R
+++ simFuns.R
@@ -1,16 +1,26 @@
 library(survival); library(frailtypack)
-# Make a trial population with a given number of clusters of a given size. Put the people in clusters; each cluster has a beta-distributed underlying antibody proportion, which is applied to the people.
+## Make a trial population with a given number of clusters of a given size. Put the people in
+## clusters, give them individual IDs and also id # within cluster
 makePop <- function(numClus=20, clusSize=300){
-	pop <- data.table(indiv=as.factor(1:(numClus*clusSize))
-		, cluster=as.numeric(gl(n=numClus, k=clusSize))
-	)
-	return(pop)
+    pop <- data.table(indiv=as.factor(1:(numClus*clusSize))
+                      , cluster=as.numeric(gl(n=numClus, k=clusSize))
+                      , idByClus = rep(1:clusSize, numClus)
+                      )
+    return(pop)
 }
 
 ## Set vaccination time for SWCT by cluster
-setSW <- function(pop, delayUnit = 7/30, immunoDelay = 21/30) {
-    pop[, vaccTime := delayUnit*(cluster-1)]
-    pop[, immuneTime := vaccTime + immunoDelay]
+setSWCTvaccDays <- function(pop, delayUnit = 7, immunoDelay = 21) {
+    pop[, vaccDay := delayUnit*(cluster-1)]
+    pop[, immuneDay := vaccDay + immunoDelay]
+    return(pop)
+}
+
+## Set vaccination time for RCT assuming same speed rollout as SWCT
+setRCTvaccDays <- function(pop, delayUnit = 7, immunoDelay = 21, clusSize) {
+    pop[idByClus <= clusSize/2, vaccDay := Inf]
+    pop[idByClus > clusSize/2, vaccDay := delayUnit*(cluster-1)]
+    pop[, immuneDay := vaccDay + immunoDelay]
     return(pop)
 }
 
@@ -32,16 +42,16 @@ setHazs <- function(pop, mu, varClus, varIndiv) {
     return(pop)
 }
 
-## Simulate SWCT
-simSWCTinfection <- function(pop, vaccEff = .8, maxInfectTime = 12) {
+## Simulate infections
+simInfection <- function(pop, vaccEff = .8, maxInfectDay = 12*30) {
     vaccRed <- 1 - vaccEff
     ## infection pre-vaccination
-    pop[, infectTime := rexp(length(indiv), rate = indivHaz)] 
-    notInfectedBeforeVacc <- with(pop, infectTime > immuneTime) 
+    pop[, infectDay := rexp(length(indiv), rate = indivHaz)] 
+    notInfectedBeforeVacc <- with(pop, infectDay > immuneDay) 
     ## infction post-vaccination
-    pop[notInfectedBeforeVacc, infectTime := immuneTime + rexp(sum(notInfectedBeforeVacc), indivHaz*vaccRed)]
-    pop[, infectTimeTrunc := infectTime]
-    pop[infectTime > maxInfectTime, infectTimeTrunc := NA]
+    pop[notInfectedBeforeVacc, infectDay := immuneDay + rexp(sum(notInfectedBeforeVacc), indivHaz*vaccRed)]
+    pop[, infectDayTrunc := infectDay]
+    pop[infectDay > maxInfectDay, infectDayTrunc := NA]
     return(pop)
 }
 
@@ -49,19 +59,18 @@ simSWCTinfection <- function(pop, vaccEff = .8, maxInfectTime = 12) {
 makeSurvDat <- function(pop) {
     ## Waiting time up until immune time or vaccination time
     st <- copy(pop)
-    st[,startTime:=0]
-    st[,endTime:=pmin(immuneTime, infectTime)]
-    st[,infected:=as.numeric(infectTime < immuneTime)]
+    st[,startDay:=0]
+    st[,endDay:=pmin(immuneDay, infectDay)]
+    st[,infected:=as.numeric(infectDay < immuneDay)]
     st[,vacc:=0]
-    st <- select(st, indiv, cluster, vaccTime, immuneTime, startTime, endTime, infected, vacc)
-    st
+    st <- select(st, indiv, cluster, idByClus, vaccDay, immuneDay, startDay, endDay, infected, vacc)
     ## For individuals who experienced vaccination time at risk, tabulate
-    st2 <- copy(pop)[infectTime > immuneTime,]
-    st2[,startTime:=immuneTime]
-    st2[, endTime := infectTime]
+    st2 <- copy(pop)[infectDay > immuneDay,]
+    st2[,startDay:=immuneDay]
+    st2[, endDay := infectDay]
     st2[, infected := 1] ## everyone gets infected eventually, but will truncate this in a separate function
     st2[, vacc := 1]
-    st2 <- select(st2, indiv, cluster, vaccTime, immuneTime, startTime, endTime, infected, vacc)
+    st2 <- select(st2, indiv, cluster, idByClus, vaccDay, immuneDay, startDay, endDay, infected, vacc)
     st <- rbind(st, st2)
     return(st)
 }
@@ -74,35 +83,48 @@ addDefArgs <- function(parms, fxn) { ## add default arguments to parameter list 
     parms[!names(parms) %in% names(formals(fxn))]
     return(parms)
 }
-## simulate whole SWCT and return with all parameters used
-simSWCT <- function(parms=list(mu=.0065, varClus=2e-05, varIndiv = 5e-06)) {
+
+## simulate whole trial and return with all parameters used
+simTrial <- function(parms=makeParms(), browse = F) {
+    if(browse) browser()
     parms$pop <- do.call(makePop, args=subsArgs(parms, makePop))
     parms <- addDefArgs(parms, makePop)
-    parms$pop <- do.call(setSW, args=subsArgs(parms, setSW))
-    parms <- addDefArgs(parms, setSW)
+    if(parms$trial=='SWCT') {
+        parms$pop <- do.call(setSWCTvaccDays, args=subsArgs(parms, setSWCTvaccDays))
+        parms <- addDefArgs(parms, setSWCTvaccDays) }
+    if(parms$trial=='RCT') {
+        parms$pop <- do.call(setRCTvaccDays, args=subsArgs(parms, setRCTvaccDays))
+        parms <- addDefArgs(parms, setRCTvaccDays)
+    }
     parms$pop <- do.call(setHazs, args=subsArgs(parms, setHazs))
     parms <- addDefArgs(parms, setHazs)
-    parms$pop <- do.call(simSWCTinfection, args=subsArgs(parms, simSWCTinfection))
-    parms <- addDefArgs(parms, simSWCTinfection)
+    parms$pop <- do.call(simInfection, args=subsArgs(parms, simInfection))
+    parms <- addDefArgs(parms, simInfection)
     parms$st <- do.call(makeSurvDat, args=subsArgs(parms, makeSurvDat))
     parms <- addDefArgs(parms, makeSurvDat)
     return(parms)
 }
 
+makeParms <- function(trial='RCT', mu=.0065, varClus=2e-05, varIndiv = 5e-06, vaccEff = .8, maxInfectDay = 12*30,
+                      numClus=20, clusSize=300){
+    list(mu=mu, varClus=varClus, varIndiv = varIndiv, trial=trial, vaccEff = vaccEff, maxInfectDay=maxInfectDay,
+         numClus=numClus, clusSize=clusSize)
+}
+
 ## Take a survival data from above function and censor it by a specified time in months
-censSurvDat <- function(st, censorTime = 6) {
-    intervalNotStarted <- st[,startTime] > censorTime
+censSurvDat <- function(st, censorDay = 6*30) {
+    intervalNotStarted <- st[,startDay] > censorDay
     st <- st[!intervalNotStarted,] 
-    noInfectionBeforeCensor <- st[,endTime] > censorTime
+    noInfectionBeforeCensor <- st[,endDay] > censorDay
     st[noInfectionBeforeCensor, infected:=0]
-    st[noInfectionBeforeCensor, endTime:=censorTime]
+    st[noInfectionBeforeCensor, endDay:=censorDay]
     return(st)
 }
 
 doCoxPH <- function(csd, browse=F) { ## take censored survival object and return vacc effectiveness estimates
-    ## mod <- coxph(Surv(startTime, endTime, infected) ~ vacc, data=csd) ## without frailty
+    ## mod <- coxph(Surv(startDay, endDay, infected) ~ vacc, data=csd) ## without frailty
     if(browse) browser()
-    modF <- coxph(Surv(startTime, endTime, infected) ~ 
+    modF <- coxph(Surv(startDay, endDay, infected) ~ 
                   vacc + frailty.gamma(cluster, eps=1e-10, method="em", sparse=0),
                   outer.max=1000, iter.max=10000,
                   data=csd)
@@ -114,40 +136,14 @@ doCoxPH <- function(csd, browse=F) { ## take censored survival object and return
 
 ## Do a binary search for the number of infections before the stopping point is reached: this is
 ## assumed to be when 95% CI of vaccine efficacy goes above 0
-firstStop <- function(parms, min=7, max=12*30, verbose = 0) { ## using days to facilitate easier rounding
+firstStop <- function(parms, min=7, max=12, verbose = 0) { ## using days to facilitate easier rounding
     if (min >= max) return(min)
     mid <- floor((min+max)/2) ## floor to days
-    lciMod <- doCoxPH(censSurvDat(parms$st, mid/30))['lci'] ## converting mid to days from months
-    if(verbose>0) print(paste0('lower 95% of vaccine efficacy at ', signif(mid/30,2), ' months =', signif(lciMod,2)))
+    lciMod <- doCoxPH(censSurvDat(parms$st, mid))['lci'] ## converting mid to days from months
+    if(verbose>0) print(paste0('lower 95% of vaccine efficacy at ', signif(mid,2), ' months =', signif(lciMod,2)))
     if(lciMod>=0)
         return(firstStop(parms, min, mid, verbose))
-    return(firstStop(parms, mid+1, max, verbose)/30) ## output in months
+    return(firstStop(parms, mid+1, max, verbose)) ## output in months
 }
 
-## # Given a model fit, return a one-tailed P value (goes from 0 for vaccine highly protective to 1 for vaccine highly risky). This is a copy from the other project, which is bad.
-## oneTailP <- function(m){
-## 	s <- summary(m)
-## 	v <- as.list(s$coef["vacc",])
-## 	if (v$z>0) return(1-v$P/2)
-## 	return(v$P/2)
-## }
-
-## rmodP <- function(people, mform){
-## 	mod <- glmer(mform, data=people,
-## 		family=binomial(link="cloglog")
-## 	)
-
-## 	if(numTrials<20) print(summary(mod))
- 
-## 	return(oneTailP(mod))
-## }
-
-## modP <- function(people, mform){
-## 	mod <- glm(mform, data=people,
-## 		family=binomial(link="cloglog")
-## 	)
-
-## 	if(numTrials<20) print(summary(mod))
- 
-## 	return(oneTailP(mod))
-## }
+casesInTrial <- function(parms, maxCaseDay = 6*30) sum(with(parms$pop, infectDay < maxCaseDay))
