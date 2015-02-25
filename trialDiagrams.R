ef2ccc1d063b96fa9177b54e48500dc76cbb8af4
Carl A. B. Pearson <blank101@gmail.com>
2015-02-25 11:34:07 -0500

    draft approach done

--- trialDiagrams.R
+++ trialDiagrams.R
@@ -6,6 +6,9 @@ addT <- 3
 
 weeks <- cats + 2 + eclipseT + addT
 
+weekFact <- factor(seq(1,weeks))
+catFact <- factor(seq(1,cats))
+
 spacing <- 0.02
 
 initHaz <- (runif(cats)+0.5)*4
@@ -18,28 +21,37 @@ hazHe <- sapply(initHaz, function(ih) ih*varyingDecline())
 dim(hazHe) <- NULL
 
 dat <- data.table(
-  week=rep(seq(1,weeks), times = cats),
-  cluster_id = rep(seq(1,cats), each = weeks),
+  week=rep(weekFact, times = cats),
+  cluster_id = rep(catFact, each = weeks),
   hazHomogeneous = hazHo,
   hazHeterogeneous = hazHe
 )
 
 
-dat[ week > (cluster_id+1), status := "eclipse"]
-dat[ week > (cluster_id+1+eclipseT), status := "vaccinated"]
+dat[ as.numeric(week) > (as.numeric(cluster_id)+1), status := "eclipse"]
+dat[ as.numeric(week) > (as.numeric(cluster_id)+1+eclipseT), status := "vaccinated"]
 dat[is.na(status), status := "unvaccinated"]
 
 dat$status <- factor(dat$status, levels=c("eclipse","vaccinated", "unvaccinated"))
 
-p <- ggplot(dat) + theme_bw() +
-  aes(xmin = week-1+spacing, xmax = week-spacing, 
-      ymin = cluster_id-1+spacing, ymax = cluster_id - spacing, fill = hazHeterogeneous) +
-  scale_fill_continuous(low="blue", high="red")
-p + scale_alpha_manual(values=c(0.7, 0.4, 1)) + geom_rect(aes(alpha=status))  ## comparison plot
+p <- ggplot(dat) +
+  aes(x=week, y=cluster_id, xmin = as.numeric(week)-0.5+spacing, xmax = as.numeric(week)+0.5-spacing, 
+      ymin = as.numeric(cluster_id)-0.5+spacing, ymax = as.numeric(cluster_id)+0.5 - spacing, fill = hazHeterogeneous) +
+  scale_fill_continuous(low="blue", high="red") +
+  theme(
+    axis.line = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank(),
+    panel.background = element_blank()
+  ) + labs(y="cluster id", fill="hazard")
+p + scale_alpha_manual(values=c(0.7, 0.4, 1)) + geom_rect(aes(alpha=status, fill=hazHomogeneous)) +
+  labs(title="SWT, homogeneous hazard shapes")  ## comparison plot
 
 p + scale_alpha_manual(values=c(0.7, 0.4)) +
   geom_rect(data=dat[status == "unvaccinated"]) +
-  geom_rect(data=dat[status != "unvaccinated"], mapping = aes(ymax=cluster_id-0.5, alpha=status)) +
-  geom_rect(data=dat[status != "unvaccinated"], mapping = aes(ymin=cluster_id-0.5))
+  geom_rect(data=dat[status != "unvaccinated"], mapping = aes(ymax=as.numeric(cluster_id), alpha=status)) +
+  geom_rect(data=dat[status != "unvaccinated"], mapping = aes(ymin=as.numeric(cluster_id))) +
+  labs(title="RCT, inhomogeneous hazard shapes")
 ## now need to get random sample of initHaz and decline rates
 
