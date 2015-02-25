00e64c07e2186eaea9e92b06c43d5ed65703f347
Carl A. B. Pearson <cap10@ufl.edu>
2015-02-24 21:48:17 -0500

    progress with triangles
    probably should shift to rects, however

--- trialDiagrams.R
+++ trialDiagrams.R
@@ -25,23 +25,23 @@ dat[ week > (cluster_id+1), status := "eclipse"]
 dat[ week > (cluster_id+1+eclipseT), status := "vaccinated"]
 dat[is.na(status), status := "unvaccinated"]
 
-dat$status <- factor(dat$status, levels=c("unvaccinated","eclipse","vaccinated"))
+dat$status <- factor(dat$status, levels=c("eclipse","vaccinated", "unvaccinated"))
 
 p <- ggplot(dat) + theme_bw() +
   aes(xmin = week-1+spacing, xmax = week-spacing, 
-      ymin = cluster_id-1+spacing, ymax = cluster_id - spacing, fill = hazHomogeneous, alpha=status) +
-  scale_alpha_discrete(range=c(1,.5,.3), labels=c("unvaccinated","eclipse","vaccinated")) + scale_fill_continuous(low="blue", high="red")
-p + geom_rect() ## comparison plot
+      ymin = cluster_id-1+spacing, ymax = cluster_id - spacing, fill = hazHomogeneous) +
+  scale_fill_continuous(low="blue", high="red")
+p + scale_alpha_manual(values=c(0.5, 0.3, 1)) + geom_rect(aes(alpha=status))  ## comparison plot
 
 dat.poly <- data.table(
   week = rep(dat$week, 3),
   cluster_id = rep(dat$cluster_id, 3),
   x = c(dat$week-1, dat$week, dat$week),
-  y=c(dat$cluster_id, dat$cluster_id, dat$cluster_id+1),
+  y=c(dat$cluster_id-1, dat$cluster_id-1, dat$cluster_id),
   hazHomogeneous = rep(dat$hazHomogeneous,3),
   status = rep(dat$status, 3)
 )
 
-p + geom_polygon(data=dat.poly, mapping = aes(group=interaction(cluster_id, week), x=x, y=y))
+p + scale_alpha_manual(values=c(0.5, 0.3)) + geom_polygon(data=dat.poly[status != "unvaccinated"], mapping = aes(group=interaction(cluster_id, week), x=x, y=y, alpha=status))
 ## now need to get random sample of initHaz and decline rates
 
