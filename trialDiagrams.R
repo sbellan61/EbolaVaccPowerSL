487251e8b4c4299529798ad5d8b84d4a21f3670f
Steve Bellan <steve.bellan@gmail.com>
2015-03-16 09:49:38 -0500

    deleted figures added FRCT risk-prioritized

--- trialDiagrams.R
+++ trialDiagrams.R
@@ -191,5 +191,5 @@ graphics.off()
 
 
 pdf('Figures/Fig SX schematic.pdf', w = 6.5, h = 7)
-multiplot(SWCT, RCTnone, OrigOrderRCTtu, FRCT, cols = 2)
+multiplot(SWCT, RCTnone, OrigOrderRCTtu, FRCTHazOrdByID, cols = 2)
 graphics.off()
